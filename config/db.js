ba3ed64e9a917c3794bbb48022724a63596534b9
atrawgn <atrawgn@gmail.com>
2023-11-05 20:00:51 +0700

    Express js #14 Menambahkan Data

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoDB = async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/praktikumkk4c');
   console.log('DB connected')
 
 }
